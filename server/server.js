a5a03c94ec77b9d40a5eaff0c727056fb904d2c2
Yu Zhang <yu.zhang39@students.mq.edu.au>
2021-10-04 12:00:15 +1100

    added "server" file to get "server" ready

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/',(require,response)=>{
+    response.json(<h1>Server is ready.</h1>)
+})
+
+const PORT = 3001
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
