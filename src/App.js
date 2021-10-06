108f550863ae2d1c5d38b45e941a0f2927b61647
Yu Zhang <yu.zhang39@students.mq.edu.au>
2021-10-06 14:47:09 +1100

    change a mistake in the "server" file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/books").then((response) => {
+      setBooks(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {
+          books.map(f =>
+              <div>{f.name}</div>
+          )
+        }
+      </div>
   );
 }
 
